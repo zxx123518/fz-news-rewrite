157c1e4d5256d41b50f2b673579916d59b281f3a
aa <aaaa>
2025-12-23 20:20:06 +0800

    初始化福州分行网讯改版项目框架：SpringBoot+Vue3，适配MySQL/高斯数据库

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+// 网讯改版项目前端基础模板
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
